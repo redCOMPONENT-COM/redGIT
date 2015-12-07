bbd5758670273af375c8f0a2f86148d8db7e1f08
Roberto Segura - phproberto <roberto@phproberto.com>
2015-12-07 21:55:16 +0100

    [imp] layout rendering system

---
+++ extensions/libraries/redgit/layout/helper.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package     Redgit.Library
+ * @subpackage  Helper
+ *
+ * @copyright   Copyright (C) 2015 redcomponent.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+use Redgit\Application;
+
+/**
+ * Layout helper.
+ *
+ * @since  1.0.0
+ */
+class RedgitLayoutHelper
+{
+	/**
+	 * Fast method to debug the layout.
+	 *
+	 * @param   string  $layoutId  Identifier of the layout to load
+	 * @param   object  $data      Object which properties are used inside the layout file to build displayed output
+	 *
+	 * @return  string
+	 */
+	public static function debug($layoutId, $data = array())
+	{
+		return Application::getRenderer($layoutId)->debug($data);
+	}
+
+	/**
+	 * Fast method to render a layout.
+	 *
+	 * @param   string  $layoutId  Identifier of the layout to load
+	 * @param   object  $data      Object which properties are used inside the layout file to build displayed output
+	 *
+	 * @return  string
+	 */
+	public static function render($layoutId, $data = array())
+	{
+		return Application::getRenderer($layoutId)->render($data);
+	}
+}
---
+++ extensions/libraries/redgit/layout/layout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Redgit.Library
+ * @subpackage  Plugin
+ *
+ * @copyright   Copyright (C) 2015 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Redgit layout renderer.
+ *
+ * @since  1.0.0
+ */
+class RedgitLayout extends RedgitLayoutFile
+{
+}
