93e051094e6870fbbcd795e425ab410d94feb100
Roberto Segura - phproberto <roberto@phproberto.com>
2015-12-07 21:51:26 +0100

    [imp] redgit system plugin

---
+++ build/joomla-gulp-extensions/plugins/system/redgit.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+
+// Load config
+var config = require('../../../gulp-config.json');
+
+// Dependencies
+var browserSync = require('browser-sync');
+var del         = require('del');
+
+var group = 'system';
+var name = 'redgit';
+
+var baseTask = 'plugins.' + group + '.' + name;
+var extPath  = '../extensions/plugins/' + group + '/' + name;
+var wwwPath  = config.wwwDir + '/plugins/' + group + '/' + name;
+
+// Clean
+gulp.task('clean:' + baseTask, function() {
+	return del(wwwPath, {force : true});
+});
+
+// Copy
+gulp.task('copy:' + baseTask, ['clean:' + baseTask], function() {
+	return gulp.src( extPath + '/**')
+		.pipe(gulp.dest(wwwPath));
+});
+
+// Watch
+gulp.task('watch:' + baseTask,
+	[
+		'watch:' + baseTask + ':plugin'
+	],
+	function() {
+});
+
+// Watch: plugin
+gulp.task('watch:' + baseTask + ':plugin', function() {
+	gulp.watch(extPath + '/**/*', ['copy:' + baseTask, browserSync.reload]);
+});
---
+++ extensions/plugins/system/redgit/redgit.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package     Redgit.Plugin
+ * @subpackage  System.Redgit
+ *
+ * @copyright   Copyright (C) 2015 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+JLoader::import('redgit.library');
+
+use Redgit\Application;
+
+/**
+ * redGIT system plugin system.
+ *
+ * @since  1.0.0
+ */
+class PlgSystemRedgit extends JPlugin
+{
+	/**
+	 * This event is triggered before the framework creates the Head section of the Document.
+	 *
+	 * @return  void
+	 */
+	public function onBeforeCompileHead()
+	{
+		$doc = Application::getDocument();
+		$doc->cleanHeader();
+	}
+}
