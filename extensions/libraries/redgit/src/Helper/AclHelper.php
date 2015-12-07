66d92235c6acbaa8b296e3c110bdc905ca6822c0
Roberto Segura - phproberto <roberto@phproberto.com>
2015-12-07 21:55:47 +0100

    [imp] acl + asset helpers

---
+++ extensions/libraries/redgit/src/Helper/AclHelper.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package     Redgit.Library
+ * @subpackage  Helper
+ *
+ * @copyright   Copyright (C) 2015 redcomponent.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Redgit\Helper;
+
+defined('_JEXEC') or die;
+
+/**
+ * ACL helper.
+ *
+ * @since  1.0.0
+ */
+class AclHelper
+{
+	/**
+	 * Gets a list of the actions that can be performed.
+	 *
+	 * @param   string  $section    The section.
+	 * @param   mixed   $component  The component name (auto = automatic)
+	 * @param   mixed   $assetName  The asset name (auto = automatic)
+	 *
+	 * @return  \JObject  The actions
+	 *
+	 * @throws  \RuntimeException
+	 */
+	public static function getActions($section = 'component', $component = 'auto', $assetName = 'auto')
+	{
+		if ('auto' === $component)
+		{
+			$component = \JApplicationHelper::getComponentName();
+		}
+
+		if (empty($component))
+		{
+			throw new \RuntimeException('Cannot detect the component name in getActions()');
+		}
+
+		if ('auto' === $assetName)
+		{
+			$assetName = $component;
+		}
+
+		$user = \JFactory::getUser();
+		$result	= new \JObject;
+		$actions = \JAccess::getActions($component, $section);
+
+		foreach ($actions as $action)
+		{
+			$result->set($action->name,	$user->authorise($action->name, $assetName));
+		}
+
+		return $result;
+	}
+}
---
+++ extensions/libraries/redgit/src/Helper/AssetHelper.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * @package     Redgit.Library
+ * @subpackage  Helper
+ *
+ * @copyright   Copyright (C) 2015 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Redgit\Helper;
+
+defined('_JEXEC') or die;
+
+/**
+ * Asset helper
+ *
+ * @since  1.0
+ */
+abstract class AssetHelper extends \JHtml
+{
+	/**
+	 * Includes assets from media directory, looking in the
+	 * template folder for a style override to include.
+	 *
+	 * @param   string  $filename   Path to file.
+	 * @param   string  $extension  Current extension name. Will auto detect component name if null.
+	 * @param   array   $attribs    Extra attribs array
+	 *
+	 * @return  mixed  False if asset type is unsupported, nothing if a css or js file, and a string if an image
+	 */
+	public static function load($filename, $extension = null, $attribs = array())
+	{
+		if ('auto' === $extension)
+		{
+			$extensionParts = explode(DIRECTORY_SEPARATOR, JPATH_COMPONENT);
+			$extension = array_pop($extensionParts);
+		}
+
+		// Try to use the directLoad function easier to debug & with direct load support
+		if ($result = static::directLoad($filename, $extension, $attribs))
+		{
+			return $result;
+		}
+
+		$toLoad = "$extension/$filename";
+
+		// Discover the asset type from the file name
+		$type = substr($filename, (strrpos($filename, '.') + 1));
+
+		switch (strtoupper($type))
+		{
+			case 'CSS':
+				return self::stylesheet($toLoad, $attribs, true, false);
+				break;
+			case 'JS':
+				return self::script($toLoad, false, true);
+				break;
+			case 'GIF':
+			case 'JPG':
+			case 'JPEG':
+			case 'PNG':
+			case 'BMP':
+				$alt = null;
+
+				if (isset($attribs['alt']))
+				{
+					$alt = $attribs['alt'];
+					unset($attribs['alt']);
+				}
+
+				return self::image($toLoad, $alt, $attribs, true);
+				break;
+			default:
+				return false;
+		}
+	}
+
+	/**
+	 * Function to add support to direct loading try to simplify all the work to be done to load an asset
+	 *
+	 * @param   string   $fileRoute           Path to file.
+	 * @param   string   $extension           Current extension name. Will auto detect component name if null.
+	 * @param   array    $attribs             Extra attribs array
+	 * @param   boolean  $searchUncompressed  Search for uncompressed files (if debug is enabled)?
+	 *
+	 * @return  mixed  False if asset type is unsupported, nothing if a css or js file, and a string if an image
+	 */
+	public static function directLoad($fileRoute, $extension = null, $attribs = array(), $searchUncompressed = true)
+	{
+		$fileName      = basename($fileRoute);
+		$fileNameOnly  = pathinfo($fileName, PATHINFO_FILENAME);
+		$fileExtension = pathinfo($fileRoute, PATHINFO_EXTENSION);
+
+		// Detect debug mode
+		if ($searchUncompressed && \JFactory::getConfig()->get('debug'))
+		{
+			/*
+			 * Detect if we received a file in the format name.min.ext
+			 * If so, strip the .min part out, otherwise append -uncompressed
+			 */
+			if (strrpos($fileNameOnly, '.min', '-4'))
+			{
+				$position = strrpos($fileNameOnly, '.min', '-4');
+				$uncompressedFileName = str_replace('.min', '.', $fileNameOnly, $position);
+				$uncompressedFileName  = $uncompressedFileName . $fileExtension;
+			}
+			else
+			{
+				$uncompressedFileName = $fileNameOnly . '-uncompressed.' . $fileExtension;
+			}
+
+			$uncompressedRoute = str_replace($fileName, $uncompressedFileName, $fileRoute);
+
+			if ($uncompressedLoad = static::directLoad($uncompressedRoute, $extension, $attribs, false))
+			{
+				return $uncompressedLoad;
+			}
+		}
+
+		$template = \JFactory::getApplication()->getTemplate();
+
+		$baseRoute = $extension ? JPATH_SITE . '/media/' . $extension : JPATH_SITE . '/media';
+		$overrideBaseRoute = $extension ? JPATH_THEMES . '/' . $template . '/' . $extension : JPATH_THEMES . '/' . $template;
+
+		$searchPaths = array(
+			dirname($overrideBaseRoute . '/' . $fileRoute),
+			dirname($overrideBaseRoute . '/' . strtolower($fileExtension) . '/' . $fileRoute),
+			dirname($baseRoute . '/' . $fileRoute),
+			dirname($baseRoute . '/' . strtolower($fileExtension) . '/' . $fileRoute),
+		);
+
+		if ($fileLocation = \JPath::find($searchPaths, $fileName))
+		{
+			$fileUrl = str_replace(JPATH_SITE, \JUri::root(true), $fileLocation);
+
+			// Windows filesystem fix
+			$fileUrl = str_replace('\\', '/', $fileUrl);
+
+			switch (strtolower($fileExtension))
+			{
+				case 'css':
+					\JFactory::getDocument()->addStylesheet($fileUrl, 'text/css', null, $attribs);
+					break;
+				case 'js':
+					\JFactory::getDocument()->addScript($fileUrl);
+					break;
+				case 'gif':
+				case 'jpg':
+				case 'jpeg':
+				case 'png':
+				case 'bmp':
+					$alt = null;
+
+					if (isset($attribs['alt']))
+					{
+						$alt = $attribs['alt'];
+						unset($attribs['alt']);
+					}
+
+					$html = '<img src="' . $fileUrl . '" alt="' . $alt . '" '
+						. trim((is_array($attribs) ? \JArrayHelper::toString($attribs) : $attribs) . ' /')
+						. '>';
+
+					return $html;
+					break;
+				default:
+					return false;
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+}
