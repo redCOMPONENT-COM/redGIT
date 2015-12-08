8a2d1909be26865cd68249d041ace5ef9e279389
Roberto Segura - phproberto <roberto@phproberto.com>
2015-12-08 04:20:56 +0100

    [imp] base list model

---
+++ extensions/libraries/redgit/src/Model/ListModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Redgit.Library
+ * @subpackage  Model
+ *
+ * @copyright   Copyright (C) 2015 redCOMPONENT.com, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Redgit\Model;
+
+defined('_JEXEC') or die;
+
+/**
+ * Model class for handling lists of items.
+ *
+ * @since  1.0.0
+ */
+class ListModel extends \JModelList
+{
+}
