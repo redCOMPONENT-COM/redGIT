0896d41bbe8b08839d4c15767b9e231c0f8eea9b
Roberto Segura - phproberto <roberto@phproberto.com>
2015-12-22 11:30:17 +0100

    [imp] mod_redgit_commit uses ajax + includes reset changes

--- extensions/components/redgit/admin/controllers/git.php
+++ extensions/components/redgit/admin/controllers/git.php
@@ -10,6 +10,7 @@
 defined('_JEXEC') or die;
 
 use Redgit\Application;
+use Redgit\Helper\AjaxHelper;
 
 /**
  * Git Controller.
@@ -40,7 +41,7 @@ class RedgitControllerGit extends JControllerLegacy
 
 			if ($git->hasChanges())
 			{
-				$git ->add('.');
+				$git->add('.');
 
 				$message = $app->input->getString(
 					'message',
@@ -64,6 +65,107 @@ class RedgitControllerGit extends JControllerLegacy
 		JFactory::getApplication()->enqueueMessage('Changes committed successfully: ' . $git->getOutput());
 
 		$this->setRedirect('index.php?option=com_redgit&view=dashboard');
+	}
+
+	/**
+	 * Discard all the current changes done in git.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   1.0.5
+	 */
+	public function ajaxCommit()
+	{
+		AjaxHelper::validateAjaxRequest();
+
+		$app = JFactory::getApplication();
+
+		if (!JFactory::getUser()->authorise('core.admin'))
+		{
+			$app->setHeader('status', 403);
+			$app->sendHeaders();
+			echo JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN');
+			$app->close();
+		}
+
+		try
+		{
+			$app = JFactory::getApplication();
+
+			$git = Application::getGit();
+
+			if ($git->hasChanges())
+			{
+				$git->add('.');
+
+				$message = $app->input->getString(
+					'message',
+					Application::getStationConfiguration()->get('default_commit_message', '[server] Latest version online')
+				);
+
+				$git->commit($message);
+			}
+
+			$git->push('origin', Application::getStationConfiguration()->get('git_branch', 'master'));
+		}
+		catch (Exception $e)
+		{
+			$app->setHeader('status', 500);
+			$app->sendHeaders();
+			echo $e->getMessage();
+			$app->close();
+		}
+
+		$app->sendHeaders();
+		echo 1;
+		$app->close();
+	}
+
+	/**
+	 * Discard all the current changes done in git.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   1.0.5
+	 */
+	public function ajaxHardReset()
+	{
+		AjaxHelper::validateAjaxRequest();
+
+		$app = JFactory::getApplication();
+
+		if (!JFactory::getUser()->authorise('core.admin'))
+		{
+			$app->setHeader('status', 403);
+			$app->sendHeaders();
+			echo JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN');
+			$app->close();
+		}
+
+		try
+		{
+			$app = JFactory::getApplication();
+
+			$git = Application::getGit();
+
+			if ($git->hasChanges())
+			{
+				$git ->add('-A');
+
+				$git->reset('--hard');
+			}
+		}
+		catch (Exception $e)
+		{
+			$app->setHeader('status', 500);
+			$app->sendHeaders();
+			echo $e->getMessage();
+			$app->close();
+		}
+
+		$app->sendHeaders();
+		echo 1;
+		$app->close();
 	}
 
 	/**
--- extensions/modules/admin/mod_redgit_commit/tmpl/default.php
+++ extensions/modules/admin/mod_redgit_commit/tmpl/default.php
@@ -9,12 +9,57 @@
 
 defined('_JEXEC') or die;
 $action = JRoute::_('index.php?option=com_redgit&task=git.commit');
+
+JHtml::_('jquery.framework');
+JHtml::script('mod_redgit_commit/commit.js', false, true, false);
 ?>
-<form action="<?php echo $action ?>" method="post" name="adminForm" id="adminForm">
-	<div class="form-group">
-		<label for="message">Commit message:</label>
-		<input id="message" type="text" class="form-control" name="message" value="<?php echo $stationConfiguration->get('default_commit_message', '[server] Latest version online'); ?>" />
+<form action="<?php echo $action ?>" method="post" name="adminForm" id="mod-redgit-commit">
+	<p class="text-muted js-mod-redgit-commit-msg"><?php echo JText::_('MOD_REDGIT_COMMIT_MSG_MODULE_LEGEND'); ?></p>
+	<!-- Reset alerts: start -->
+	<div class="alert alert-info js-mod-redgit-commit-msg-confirm-reset" style="display: none;">
+		<p style="margin-bottom: 15px;"><?php echo JText::_('MOD_REDGIT_COMMIT_MSG_CONFIRM_RESET'); ?></p>
+		<div>
+			<button class="btn btn-success js-mod-redgit-commit-btn-confirm-reset" type="button"><?php echo JText::_('JYES'); ?></button>
+			<button class="btn btn-danger js-mod-redgit-commit-btn-cancel-reset" type="button"><?php echo JText::_('JNO'); ?></button>
+		</div>
 	</div>
-	<button class="btn btn-default" type="submit"><?php echo JText::_('MOD_REDGIT_COMMIT_LINK_COMMIT'); ?></button>
+	<div class="alert alert-info js-mod-redgit-commit-msg-wip-reset" style="display: none;">
+		<i class="fa fa-spinner fa-pulse"></i> <?php echo JText::_('MOD_REDGIT_COMMIT_MSG_RESETING_REPOSITORY'); ?>
+	</div>
+	<div class="alert alert-success js-mod-redgit-commit-msg-success-reset" style="display: none;">
+		<p style="margin-bottom: 15px;"><?php echo JText::_('MOD_REDGIT_COMMIT_MSG_RESET_SUCCESS'); ?></p>
+		<button class="btn btn-default js-mod-redgit-commit-btn-reload" type="button"><?php echo JText::_('MOD_REDGIT_COMMIT_LINK_RETURN'); ?></button>
+	</div>
+	<!-- Reset alerts: end -->
+	<!-- Commit alerts: start -->
+	<div class="alert alert-info js-mod-redgit-commit-msg-confirm-commit" style="display: none;">
+		<p style="margin-bottom: 15px;"><?php echo JText::_('MOD_REDGIT_COMMIT_MSG_CONFIRM_COMMIT'); ?></p>
+		<div>
+			<button class="btn btn-success js-mod-redgit-commit-btn-confirm-commit" type="button"><?php echo JText::_('JYES'); ?></button>
+			<button class="btn btn-danger js-mod-redgit-commit-btn-cancel-commit" type="button"><?php echo JText::_('JNO'); ?></button>
+		</div>
+	</div>
+	<div class="alert alert-info js-mod-redgit-commit-msg-wip-commit" style="display: none;">
+		<i class="fa fa-spinner fa-pulse"></i> <?php echo JText::_('MOD_REDGIT_COMMIT_MSG_COMMITING_DATA'); ?>
+	</div>
+	<div class="alert alert-success js-mod-redgit-commit-msg-success-commit" style="display: none;">
+		<p style="margin-bottom: 15px;"><?php echo JText::_('MOD_REDGIT_COMMIT_MSG_COMMIT_SUCCESS'); ?></p>
+		<button class="btn btn-default js-mod-redgit-commit-btn-reload" type="button"><?php echo JText::_('MOD_REDGIT_COMMIT_LINK_RETURN'); ?></button>
+	</div>
+	<!-- Commit alerts: end -->
+	<div class="alert alert-danger js-mod-redgit-commit-msg-error" style="display: none;">
+		<h4><?php echo JText::_('MOD_REDGIT_COMMIT_MSG_ERROR_WAS_FOUND'); ?></H4>
+		<p class="js-mod-redgit-commit-msg-error-message" style="margin-bottom: 15px;"></p>
+		<button class="btn btn-default js-mod-redgit-commit-btn-return" type="button"><?php echo JText::_('MOD_REDGIT_COMMIT_LINK_RETURN'); ?></button>
+	</div>
+
+	<div class="js-mod-redgit-commit-actions">
+		<div class="form-group">
+			<label for="message">Commit message:</label>
+			<input id="message" type="text" class="form-control" name="message" value="<?php echo $stationConfiguration->get('default_commit_message', '[server] Latest version online'); ?>" />
+		</div>
+		<button class="btn btn-default js-mod-redgit-commit-btn-reset" type="button"><?php echo JText::_('MOD_REDGIT_COMMIT_LINK_HARD_RESET'); ?></button>
+		<button class="btn btn-default js-mod-redgit-commit-btn-commit" type="button"><?php echo JText::_('MOD_REDGIT_COMMIT_LINK_COMMIT'); ?></button>
+	</div>
 	<?php echo JHtml::_('form.token'); ?>
 </form>
